1016dfb50e06d5d7139fb57e229287da7de6751c
rengreen <r.m.gardzinska@gmail.com>
2019-01-01 21:26:27 +0100

    cutting cyclic part & marking happy numbers

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<Long> cycle = new ArrayList<>();
+        Collections.addAll(cycle, 4L, 16L, 37L, 58L, 89L, 145L, 42L, 20L);
 
         for (long start=1; start<=20; start++) {
             long number = start;
             long sum = 0;
             int digit = 0;
-            for (int i = 1; i < 50; i++) {
+
+            do {
+                System.out.print(number + " ");
                 sum = 0;
                 while (number > 0) {
                     digit = (int) (number % 10);
@@ -16,7 +23,10 @@ public class Main {
                     number /= 10;
                 }
                 number = sum;
-                System.out.print(number + " ");
+            } while(number!=1 && !cycle.contains(number));
+
+            if (number==1L){
+                System.out.print(" happy number");
             }
             System.out.println("");
         }
