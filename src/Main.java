4951e0b5949109b8d54bd2631a729a8fb4a5ec45
rengreen <r.m.gardzinska@gmail.com>
2019-01-01 20:26:55 +0100

    sequence for single start number

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.HashSet;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        long number = 18;
+        long sum = 0;
+        int digit = 0;
+        for (int i=1; i<50;i++) {
+            sum = 0;
+            while (number > 0) {
+                digit = (int) (number % 10);
+                sum += digit * digit;
+                number /= 10;
+            }
+            number = sum;
+            System.out.print(number+" ");
+        }
     }
 }
