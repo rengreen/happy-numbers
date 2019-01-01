669de098ff5daaa729a3459933c1b6da40b789e1
rengreen <r.m.gardzinska@gmail.com>
2019-01-01 20:34:59 +0100

    loop with subsequent starting numbers

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        long number = 18;
-        long sum = 0;
-        int digit = 0;
-        for (int i=1; i<50;i++) {
-            sum = 0;
-            while (number > 0) {
-                digit = (int) (number % 10);
-                sum += digit * digit;
-                number /= 10;
+        for (long start=1; start<=20; start++) {
+            long number = start;
+            long sum = 0;
+            int digit = 0;
+            for (int i = 1; i < 50; i++) {
+                sum = 0;
+                while (number > 0) {
+                    digit = (int) (number % 10);
+                    sum += digit * digit;
+                    number /= 10;
+                }
+                number = sum;
+                System.out.print(number + " ");
             }
-            number = sum;
-            System.out.print(number+" ");
+            System.out.println("");
         }
     }
 }
